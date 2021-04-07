a5fda2815e56ccd6480a88041d376b36aa6859c5
Gisele Dias <gisaklc@hotmail.com>
2021-04-07 13:21:40 -0300

    criação da camada de acesso a dados respository e servico

---
+++ src/main/java/com/gisaklc/cursomc/services/CategoriaService.java
@@ -0,0 +1,28 @@
+package com.gisaklc.cursomc.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.gisaklc.cursomc.domain.Categoria;
+import com.gisaklc.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+	public List<Categoria> findAll() {
+		List<Categoria> lista = repo.findAll();
+		return lista;
+	}
+
+}
