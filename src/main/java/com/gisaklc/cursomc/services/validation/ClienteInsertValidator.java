a7f818742ac5e53bdc872722f29055d18872d844
Gisele Dias <gisaklc@hotmail.com>
2021-05-05 17:36:08 -0300

    validacao email nao repetido na inserção do cliente

--- src/main/java/com/gisaklc/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/gisaklc/cursomc/services/validation/ClienteInsertValidator.java
@@ -6,18 +6,27 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.gisaklc.cursomc.domain.Cliente;
 import com.gisaklc.cursomc.domain.enums.TipoCliente;
 import com.gisaklc.cursomc.dto.ClienteNewDTO;
+import com.gisaklc.cursomc.repositories.ClienteRepository;
 import com.gisaklc.cursomc.resource.exception.FieldMessage;
 import com.gisaklc.cursomc.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
 
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
 		List<FieldMessage> list = new ArrayList<>();
 
 		if (objDto.getTipoClienteEnum().equals(TipoCliente.PESSOAFISICA.getCod())
@@ -31,6 +40,13 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ inválido"));
 		}
+		Cliente cli = repo.findByEmail(objDto.getEmail());
+
+		if (cli != null) {
+
+			list.add(new FieldMessage("email", "Já existe o email cadastro"));
+		}
+
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getName())
