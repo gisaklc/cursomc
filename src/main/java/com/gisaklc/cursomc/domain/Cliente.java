136950df04e50f4354fe7ac472d0a17fb4cefe82
Gisele Dias <gisaklc@hotmail.com>
2021-05-05 14:49:58 -0300

    deletanto um cliente

--- src/main/java/com/gisaklc/cursomc/domain/Cliente.java
+++ src/main/java/com/gisaklc/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
@@ -29,7 +30,8 @@ public class Cliente implements Serializable {
 	private String email;
 	private Integer tipoClienteEnum;
 
-	@OneToMany(mappedBy = "cliente")
+	//cascade apaga em cascada os endereço caso exclua o cliente
+	@OneToMany(mappedBy = "cliente", cascade=CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
 
 	@ElementCollection
