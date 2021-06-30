d5d089176669e3612fd4218e04ee37339b0d8a48
Gisele Dias <gisaklc@hotmail.com>
2021-06-30 09:34:10 -0300

    criação do procfile de produção

--- src/main/java/com/gisaklc/cursomc/config/DevConfig.java
+++ src/main/java/com/gisaklc/cursomc/config/DevConfig.java
@@ -3,6 +3,7 @@ package com.gisaklc.cursomc.config;
 import java.text.ParseException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -16,13 +17,17 @@ public class DevConfig {
 	@Autowired
 	private DbService dbService;
 
-	@Value(="${spring.jpa.hibernate.ddl-auto=create}")
+	@Value("${spring.jpa.hibernate.ddl-auto}")
 	private String strategy;
 	
 	// metodo responsavel por instanciar o bd no profile test
 	@Bean
 	public boolean instantiateDatabase() throws ParseException {
 
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
 		// popula a base de dados
 		dbService.instantiateTestDatabase();
 
