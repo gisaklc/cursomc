774818c9c8ae07ca047bbf3bbf88e78508b93f8a
gisaklc <gisaklc@hotmail.com>
2021-08-30 12:15:15 -0300

    Salvando arquivos no s3 amazon

--- src/main/java/com/gisaklc/cursomc/config/SecurityConfig.java
+++ src/main/java/com/gisaklc/cursomc/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.gisaklc.cursomc.config;
 
-import java.util.Arrays;
+ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/com/gisaklc/cursomc/resource/exception/FileException.java
@@ -0,0 +1,15 @@
+package com.gisaklc.cursomc.resource.exception;
+
+public class FileException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public FileException(String msg) {
+		super(msg);
+	}
+
+	public FileException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+
+}
